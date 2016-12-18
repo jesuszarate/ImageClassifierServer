bb916f805e87740248825b9f3e5115912988edd6
Jesus <chuy8jay@gmail.com>
2016-12-17 20:30:48 -0700

    Changed wsgi to avoid error

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,20 +10,12 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 
-#from django.core.wsgi import get_wsgi_application
-#from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-#import django.core.handler.wsgi
-
-#from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(get_wsgi_application())
-
-application = django.core.handlers.wsgi.WSGIHandler()
-
-#application = get_wsgi_application()
-from dj_static import Cling
+application = get_wsgi_application()
+#from dj_static import Cling
 application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
