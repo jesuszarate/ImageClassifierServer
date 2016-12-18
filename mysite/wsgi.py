058d8f0534eff384a8b72dcaeffc99fee1bcc33f
Jesus <chuy8jay@gmail.com>
2016-12-17 20:42:18 -0700

    Changed wsgi to avoid error

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -17,5 +17,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
 #from dj_static import Cling
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
