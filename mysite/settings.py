f4271c373011fd0b1d7e6750905cdb6b7b2b6e87
Jesus <chuy8jay@gmail.com>
2016-12-17 20:38:13 -0700

    Changed wsgi to avoid error

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,8 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 
-with open('../secret.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = os.environ['SECRET_KEY']
+
+#with open('../secret.txt') as f:
+#    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
