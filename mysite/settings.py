e13f38ff4644d58a19b79f45dcd4fb6094713af5
Jesus <chuy8jay@gmail.com>
2017-01-10 12:13:39 -0700

    removed debug mode

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 if DEBUG:
     with open('../secret.txt') as f:
