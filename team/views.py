9db56aa1e3e92a00d0a44f2f6b2bb391579353fc
Jesus <chuy8jay@gmail.com>
2017-08-09 00:26:15 -0600

    debugging

--- team/views.py
+++ team/views.py
@@ -50,7 +50,7 @@ class UploadFileForm(forms.Form):
 
 def handle_uploaded_file(f):
     #/Users/jesuszarate/GitHub/DjangoTutorial/mysite/team/Classifier/images
-    with open('./team/Classifier/images/name.jpg', 'wb+') as destination:
+    with open('team/Classifier/images/name.jpg', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
